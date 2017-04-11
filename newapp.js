22d3e3360a873665b6615e8577f4013ba044a74f
Izabella Arya <izabellabaer1@Izabella-Baers-MacBook-Pro.local>
2017-04-11 14:13:11 -0700

    added one constructor and two methods

--- newapp.js
+++ newapp.js
@@ -6,6 +6,8 @@ var seattleCenterUl = document.getElementById('seattlecenter');
 var alkiUl = document.getElementById('alki');
 var seatacAirportUl = document.getElementById('seatacairport');
 
+var storeHours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
+
 
 function Store(title){
   this.title = title;
@@ -18,5 +20,13 @@ function Store(title){
 }
 
 Store.prototype.randomCustNum = function(){
-  
+  return Math.floor(Math.random()* (65 - 23 + 1)) + 23;
+};
+
+Store.prototype.cookiesPerHour = function(){
+  var locationLi;
+  for (var i = 0; i < 15; i++) {
+    var totalCookiesPerHour = Math.floor(this.avgCookiesPurch * this.randomCustNum());
+    this.locationResults.push(storeHours[i] + ' = ' + totalCookiesPerHour);
+    this.cookieBasket = totalCookiesPerHour + this.cookieBasket;
 };

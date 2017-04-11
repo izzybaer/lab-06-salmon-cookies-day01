fd29926722e0666501b1d1974d9752a9f3bd72b3
Izabella Arya <izabellabaer1@Izabella-Baers-MacBook-Pro.local>
2017-04-11 13:55:01 -0700

    created store object constructor

--- newapp.js
+++ newapp.js
@@ -1,1 +1,22 @@
 'use strict';
+
+var firstAndPikeUl = document.getElementById('firstandpike');
+var capitolHillUl = document.getElementById('capitolhill');
+var seattleCenterUl = document.getElementById('seattlecenter');
+var alkiUl = document.getElementById('alki');
+var seatacAirportUl = document.getElementById('seatacairport');
+
+
+function Store(title){
+  this.title = title;
+  this.cookieBasket = 0;
+  this.locationResults = [];
+  this.hoursOpen = 15;
+  this.minHourlyCust = minHourlyCust;
+  this.maxHourlyCust = maxHourlyCust;
+  this.avgCookiesPurch = avgCookiesPurch;
+}
+
+Store.prototype.randomCustNum = function(){
+  
+};
